18861579d6f6e77a425a9d5f35822c9e61e4d425
Juanita Blignaut <jblignautbc@gmail.com>
2025-01-27 09:02:39 +0200

    adding more than one weather card

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -2,10 +2,11 @@ const url ='https://api.openweathermap.org/data/2.5/weather';
 const apiKey ='f00c38e0279b7bc85480c3fe775d518c';
 
 $(document).ready(function(){
-    weatherFin('Johannesburg');
+    defaultweatherFin('Johannesburg');
+    weatherFin('');
 });
 
-async function weatherFin(city){
+async function defaultweatherFin(city){
     const temp = `${url}?q=${city}&appid=${apiKey}&units=metric`;
 
     try{
@@ -22,11 +23,34 @@ async function weatherFin(city){
 }
 
 function weatherShowFin(data){
-    $('#city_name').text(data.name);
-    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    $('#temp').html(`${data.main.temp} &deg;C`);
-    $('#description').text(data.weather[0].description);
-    $('#wind_speed').html(`Wind Speed: ${data.wind.speed} m/s`);
-    $('#weather_icon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
-    $('#w_info').fadeIn();
+    // Create a new weather card
+    const weatherCard = `
+        <div class="animate_A animate_fadeIn w_info">
+            <h3>${data.name}</h3>
+            <p>${moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
+            <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">
+            <p>${data.main.temp} &deg;C</p>
+            <p>${data.weather[0].description}</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+        </div>
+    `;
+    
+    // Append the new card to the container
+    $('#weather_cards_container').append(weatherCard);
+}
+
+async function weatherFin(city){
+    const temp = `${url}?q=${city}&appid=${apiKey}&units=metric`;
+
+    try{
+        const res = await fetch(temp);
+        const data = await res.json();
+        if(res.ok){
+            weatherShowFin(data); // Call to add a new card
+        }else{
+            alert('City not found. \n Please try again');
+        }
+    } catch(err){
+        console.error('Error fetching weather data: ', err);
+    }
 }
